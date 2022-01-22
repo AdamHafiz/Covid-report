558d68fe6f16aa1a32969151de4843dd9e16a950
AdamHafiz <adamhafizabhadi78@gmail.com>
2022-01-22 16:39:18 +0800

    add world report func

--- app.py
+++ app.py
@@ -29,23 +29,24 @@ def generate_world_report():
     json_data = file.read()
 
   data = json.loads(json_data)
+  data_active_cases = data["item19"]
+  data_deathrate = data["item20"]
   data_total_cases = data["item21"]
-  data_daily_cases = data["item22"]
-  data_active_cases = data["item23"]
-  data_total_deaths = data["item24"]
-  data_daily_deaths = data["item25"]
-
-
+  data_total_deaths = data["item22"]
+  
   print(data_total_cases["name1"], data_total_cases["x_axis"][-1], data_total_cases["data1"][-1])
+
   for n in range(7):
-    print(data_daily_cases["name1"], data_daily_cases["x_axis"][-(n+1)], data_daily_cases["data1"][-(n+1)])
-  print(data_active_cases["name1"], data_active_cases["x_axis"][-1], data_active_cases["data1"][-1])
+    print(data_active_cases["name1"], data_active_cases["x_axis"][-1], data_active_cases["data1"][-1])
+    
   print(data_total_deaths["name1"], data_total_deaths["x_axis"][-1], data_total_deaths["data1"][-1])
+
   for n in range(7):
-    print(data_daily_deaths["name1"], data_daily_deaths["x_axis"][-(n+1)], data_daily_deaths["data1"][-(n+1)])
+    print(data_deathrate["name1"], data_deathrate["x_axis"][-(n+1)], data_deathrate["data1"][-(n+1)])
 
 
 generate_malaysia_report()
+generate_world_report()
 
 # yag = yagmail.SMTP('violettulip00@gmail.com', 'sdssdsdsdsds')
 # contents = ['This is the body, and here is just text http://somedomain/image.png',
